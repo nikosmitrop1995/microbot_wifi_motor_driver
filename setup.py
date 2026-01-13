f493a20668bcc7474978455a10a110f63d64d1ac
Nikos Mitropoulos <nikosmitrop1995@gmail.com>
2026-01-13 15:35:23 +0200

    chore: bump version to 0.1.0 in package.xml and setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
+"""Setup configuration for microbot_wifi_motor_driver package."""
 import os
 from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = 'microbot_wifi_motor_driver'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
